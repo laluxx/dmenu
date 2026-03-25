da468470de5a19defd8a375258e0dc0bf788a53e
laluxx <lorenzoaddamo10@gmail.com>
2026-03-25 14:10:14 +0100

    update

--- config.def.h
+++ config.def.h
@@ -5,16 +5,18 @@ static const char *fonts[] = {
 };
 
 static const char *prompt      = NULL;
-static const char *colors[SchemeLast][2] = {
-        //                fg         bg
-        [SchemeNorm] = { "#F0EBE7", "#2B1D2B" },
-        [SchemeSel]  = { "#2B1D2B", "#C68EDE" },
-        [SchemeOut]  = { "#000000", "#00ffff" },
+static const char *colors[SchemeLast][3] = {
+    //                fg         bg
+    [SchemeNorm]   = { "#F0EBE7", "#2B1D2B" },
+    [SchemeSel]    = { "#2B1D2B", "#C68EDE" },
+    [SchemeOut]    = { "#000000", "#00ffff" },
+    [SchemeCursor] = { "#2B1D2B", "#9587DD" }, // Character color, Cursor block color
 };
 
-static unsigned int lines = 0;
-static unsigned int maxhist    = 64;
-static int histnodup           = 1;   // if 0, record repeated histories
+static unsigned int lines       = 0;
+static unsigned int cursorwidth = 14;
+static unsigned int maxhist     = 64;
+static int          histnodup   = 1;   // if 0, record repeated histories
 
 /*
  * Characters not considered part of a word while deleting words
